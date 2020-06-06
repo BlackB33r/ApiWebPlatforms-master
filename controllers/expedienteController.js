30863486888fc7a698560bd0a134ef6362207581
L Mario <marioxns07@gmail.com>
2020-06-05 20:05:51 -0600

    first commit

---
+++ controllers/expedienteController.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const Expediente = require('../models/expediente');
+const async = require('async');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+
+//login = email y password
+
+const jwtKey = config.get("secret.key");
+//user => email, name, last_name, password
+
+// todos los elementos GET / => list
+function list(req, res, next) {
+  res.status(200).json({
+   message:'Usuarios del sistema'
+  })
+}
+
+// regrese un elemento GET /:id => index
+function index(req, res, next) {
+  res.status(200).json({
+   message:'Usuarios del sistema'
+ })
+}
+
+// crea un elemento POST /create => createError
+function create(req, res, next) {
+  res.status(200).json({
+   message:'Usuarios del sistema'
+ })
+}
+
+// modifica un elemento PUT /:id => update
+function update(req, res, next) {
+  res.status(200).json({
+   message:'Usuarios del sistema'
+  })
+}
+
+//elimina un elemento DELETE /:id => destroy
+function destroy(req, res, next) {
+  res.status(200).json({
+   message:'Usuarios del sistema'
+  })
+}
+
+module.exports = {
+  list, index, create, update, destroy
+}
---
+++ routes/expedientes.js
@@ -0,0 +1,17 @@
+var express = require('express');
+const expedienteController = require('../controllers/expedienteController')
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/index/:id', expedienteController.index);
+
+router.get('/:page?', expedienteController.list);
+
+router.post('/', expedienteController.create);
+
+router.put('/:id', expedienteController.update);
+
+router.delete('/:id', expedienteController.destroy);
+
+
+module.exports = router;
